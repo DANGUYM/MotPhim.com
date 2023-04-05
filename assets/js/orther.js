742498471afae3704bbf176cd2a38024441c3b9a
KHANG DORA <Khangdora>
2023-04-05 19:30:24 +0700

    update file

--- assets/js/orther.js
+++ assets/js/orther.js
@@ -2,15 +2,30 @@
 function orther(number) {
     if(number == 1) {
 
-        var category = document.getElementById('category');
-        category.addEventListener('click', function() {
-            if(document.getElementById('list-category').style.display == "none") 
-                document.getElementById('list-category').style.display = "grid";
-            else 
-                document.getElementById('list-category').style.display = "none";
-        })
+        //Trang thể loại
 
-        $.getJSON("https://cors-anywhere.herokuapp.com/https://motphimz.000webhostapp.com/assets/js/category.json", function(data){
+        function chooseFilter(x,y) {
+
+            document.getElementById("list-category").style.display = "none";
+
+            var status = document.getElementById(x);
+
+            status.addEventListener('click', function() {
+                if(document.getElementById(y).style.display == "none") 
+                    document.getElementById(y).style.display = "grid";
+                else 
+                    document.getElementById(y).style.display = "none";
+            })
+        }
+
+        chooseFilter('category','list-category');
+        chooseFilter('status','list-status');
+        chooseFilter('movie','list-movie');
+        chooseFilter('sort','list-sort');
+        
+        //https://cors-anywhere.herokuapp.com/https://motphimz.000webhostapp.com/assets/js/category.json
+
+        $.getJSON("https://motphimz.000webhostapp.com/assets/js/category.json", function(data){
             var items = [];
             $.each(data, function(key, val){
               items.push('<li class="dropdown-category-items">' +
@@ -22,6 +37,17 @@ function orther(number) {
 
     }
 
+    if(number == 2) {
+
+        //Trang tìm kiếm
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const q = urlParams.get('q');
+        if(q==""||q==null) {
+            window.location.href="./index.html";
+        }
+        document.getElementById("result-search").innerHTML = q;
 
 
+    }
 }
