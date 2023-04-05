3c00c7a13681d78716d98ac97872467c0bf88507
KHANG DORA <Khangdora>
2023-04-05 23:31:05 +0700

    form đăng ký/đăng nhập

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let theme;
 var user_data = document.getElementById('user-avatar');
 var menu_mobile = document.getElementById('menu-mobile');
 var remove_user = document.getElementById('remove-user');
+var removie_menu = document.getElementById('remove-menu');
 var navbar_user = document.getElementById('navbar-user');
 var navbar_menu = document.getElementById('navbar-menu');
 var change_theme = document.getElementById('change-theme');
@@ -15,9 +16,9 @@ var change_theme = document.getElementById('change-theme');
 // tài khoản 3 admin / 123456 / tài khoản quản trị
 
 if(localStorage.hasOwnProperty("username")) {
-    
+
 }else {
-    document.getElementById("navbar-user").innerHTML = "";
+    document.getElementById("navbar-user").innerHTML = '<div class="profile"> <img class="navbar-user-avatar" src="./assets/img/user.png"/> <div class="navbar-user-name">Chào mừng bạn!</div> <div class="navbar-user-times" id="remove-user" onclick="removeuser()"><i class="fa fa-times"></i></div> </div> <div class="navbar-tabs"> <div class="navbar-tab-item actived" id="login" onclick="login();">Đăng nhập</div> <div class="navbar-tab-item" id="signup" onclick="signup();">Đăng ký</div> </div> <div class="navbar-content" id="content-login"> <div class="navbar-content-item"> <label class="navbar-lbl">Tên tài khoản</label> <input class="navbar-input" type="text" name="username" id="username" /> <i class="fa fa-user-o"></i> <span class="navbar-tip">từ 3 - 20 ký tự</span> </div> <div class="navbar-content-item"> <label class="navbar-lbl">Mật khẩu</label> <input class="navbar-input" type="password" name="password" id="password" /> <i class="fa fa-lock"></i> <span class="navbar-tip">từ 6 - 30 kí tự</span> </div> <div class="navbar-content-item"> <label class="navbar-lbl"><input type="checkbox" checked/> Ghi nhớ</label> <a href="" class="forgot-password">Quên mật khẩu?</a> </div> <div class="navbar-content-item"> <button class="navbar-btn" name="login">Đăng nhập</button> </div> <div class="navbar-content-item"> <button class="navbar-btn facebook" name="facebook">Đăng nhập với Facebook</button> </div> </div> <div class="navbar-content" id="content-signup"> <div class="navbar-content-item"> <label class="navbar-lbl">Tên tài khoản</label> <input class="navbar-input" type="text" name="username" id="username" /> <i class="fa fa-user-o"></i> <span class="navbar-tip">từ 3 - 20 ký tự</span> </div> <div class="navbar-content-item"> <label class="navbar-lbl">Mật khẩu</label> <input class="navbar-input" type="password" name="password" id="password" /> <i class="fa fa-lock"></i> <span class="navbar-tip">từ 6 - 30 kí tự</span> </div> <div class="navbar-content-item"> <label class="navbar-lbl">Nhập lại mật khẩu</label> <input class="navbar-input" type="password" name="password2" id="password2" /> <i class="fa fa-lock"></i> </div> <div class="navbar-content-item"> <label class="navbar-lbl">Tên hiển thị</label> <input class="navbar-input" type="text" name="name" id="name" /> <i class="fa fa-comments"></i> <span class="navbar-tip">từ 6 - 30 kí tự</span> </div> <div class="navbar-content-item"> <label class="navbar-lbl">Email</label> <input class="navbar-input" type="email" name="email" id="email" /> <i class="fa fa-envelope-o"></i> </div> <div class="navbar-content-item"> <button class="navbar-btn" name="login">Đăng ký</button> </div> </div>';
 }
 
 if(localStorage.hasOwnProperty("theme")) {
@@ -50,24 +51,36 @@ function showSlides(){
 
 }
 
+function signup() {
+    document.getElementById("content-login").style.display = "none";
+    document.getElementById("content-signup").style.display = "block";
+    document.getElementById("login").classList.remove("actived");
+    document.getElementById("signup").classList.add("actived");
+}
+
+function login() {
+    document.getElementById("content-login").style.display = "block";
+    document.getElementById("content-signup").style.display = "none";
+    document.getElementById("login").classList.add("actived");
+    document.getElementById("signup").classList.remove("actived");
+}
+
 menu_mobile.addEventListener('click', function() {
 
-    if(navbar_menu.style.width === "0px" || navbar_menu.style.display === "none") {
-        navbar_menu.style.width = "300px";
-        navbar_menu.style.visibility = "unset";
+    if(navbar_menu.style.left === "-300px") {
+        navbar_menu.style.left = "0px";
     }else {
-        navbar_menu.style.visibility = "hidden";
-        navbar_menu.style.width = "0px";
+        navbar_menu.style.left = "-300px";
     }
 
 });
 
 user_data.addEventListener('click', function() {
 
-    if(navbar_user.style.width === "0px" || navbar_user.style.display === "none") {
-        navbar_user.style.width = "300px";
+    if(navbar_user.style.right === "-300px") {
+        navbar_user.style.right = "0px";
     }else {
-        navbar_user.style.width = "0px";
+        navbar_user.style.right = "-300px";
     }
 
 });
@@ -77,9 +90,13 @@ user_data.addEventListener('click', function() {
 //     var user_name = document.getElementsByClassName("navbar-user-name").innnerHTML(cookieValue);
 // }
 
-remove_user.addEventListener('click', function() {
-    navbar_user.style.width = "0px";
+removie_menu.addEventListener('click', function() {
+    navbar_menu.style.left = "-300px";
 });
+
+function removeuser() {
+    navbar_user.style.right = "-300px";
+}
 
 change_theme.addEventListener('click', function() {
 
